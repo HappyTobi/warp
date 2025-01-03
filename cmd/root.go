70722049c0f5931a8223b91a8db7efa1da556ab5
Tobias Schug <happytobi@tscoding.de>
2025-01-03 22:43:13 +0100

    chore: configuration file

--- cmd/root.go
+++ cmd/root.go
@@ -1,27 +1,24 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/HappyTobi/warp/pkg/cmd/settings"
+	"github.com/HappyTobi/warp/pkg/cmd/configuration"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-)
-
-const (
-	oldConfig      = ".warp.yaml"
-	configFileName = "warp.yaml"
 )
 
 func Root() *cobra.Command {
 	root := &cobra.Command{
 		Use: "warp",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			//load the configuration file
+			configPath, _ := cmd.Flags().GetString("config")
+			return configuration.ReadConfig(configPath)
+		},
 	}
 
 	root.PersistentFlags().StringP("charger", "c", "", "Url to Warp charger like http://192.168.1.2 or http://warp.local")
 	root.PersistentFlags().StringP("output", "o", "json", "Output format (json, yaml)")
+
+	root.PersistentFlags().String("config", "", "Path to the warp configuration file (default $HOME/.config/warp/warp.yaml)")
 
 	root.PersistentFlags().StringP("username", "u", "", "Username to authenticate (required if password is set)")
 	root.PersistentFlags().StringP("password", "p", "", "Password to authenticate (required if username is set)")
@@ -34,79 +31,12 @@ func Root() *cobra.Command {
 	root.AddCommand(MeterCmd())
 	root.AddCommand(EvccCmd())
 	root.AddCommand(VersionCmd())
+	root.AddCommand(ConfigurationCmd())
 
 	return root
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-// TODO move to settings package
-func initConfig() {
-	home, err := os.UserHomeDir()
-	cobra.CheckErr(err)
-
-	configPath := filepath.Join(home, ".config", "warp")
-	configFilePath := filepath.Join(configPath, configFileName)
-	configFilePathLegacy := filepath.Join(home, oldConfig)
-
-	migrated := false
-	if _, err := os.Stat(configFilePathLegacy); !os.IsNotExist(err) {
-		fmt.Println("Configurations file will me moved and updated to new format")
-		_ = os.MkdirAll(configPath, os.ModePerm)
-		if err := os.Rename(configFilePathLegacy, configFilePath); err != nil {
-			fmt.Println("Error while moving config file")
-			os.Exit(1)
-		}
-		os.Remove(configFilePathLegacy)
-		migrated = true
-	}
-
-	viper.AddConfigPath(configPath)
-	viper.SetConfigType("yaml")
-	viper.SetConfigName("warp")
-
-	if err := viper.ReadInConfig(); err == nil && !migrated {
-		return
-	}
-
-	viper.SetDefault("settings.user.firstname", "happy")
-	viper.SetDefault("settings.user.lastname", "tobi")
-	viper.SetDefault("settings.user.street", "githubroad")
-	viper.SetDefault("settings.user.postcode", "0000")
-	viper.SetDefault("settings.user.city", "internet")
-
-	viper.SetDefault("settings.date_time.time_zone", "Europe/Berlin")
-	viper.SetDefault("settings.date_time.time_format", "15:04:05 02-01-2006")
-	viper.SetDefault("settings.power.price", "0.35")
-
-	//csv settings
-	viper.SetDefault("csv.comma", ";")
-	viper.SetDefault("csv.header", true)
-
-	//pdf settings
-	viper.SetDefault("pdf.print_header", false)
-	imagePath := filepath.Join(configPath, "logo.png")
-	viper.SetDefault("pdf.image_path", imagePath)
-
-	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		_ = os.MkdirAll(configPath, os.ModePerm)
-		if err := os.WriteFile(configFilePath, []byte{}, os.ModePerm); err != nil {
-			fmt.Print("Error while creating config file")
-			os.Exit(1)
-		}
-	}
-
-	if err := settings.StoreImage(imagePath); err != nil {
-		fmt.Print("Error while storing image")
-		os.Exit(1)
-	}
-
-	if err := viper.WriteConfig(); err != nil {
-		fmt.Print(err)
-		os.Exit(1)
-	}
-
-	fmt.Println("New warp config file created at:", viper.ConfigFileUsed())
+	// enable all pre functions
+	cobra.EnableTraverseRunHooks = true
 }
--- pkg/cmd/middleware/flags.go
+++ pkg/cmd/middleware/flags.go
@@ -6,6 +6,7 @@ import (
 	"github.com/HappyTobi/warp/pkg/internal/renderer"
 	"github.com/HappyTobi/warp/pkg/internal/warp"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func LoadWarpRequest(cmd *cobra.Command) (warp.Request, error) {
@@ -34,7 +35,20 @@ func LoadGlobalParams(cmd *cobra.Command, req func(charger, username, password, 
 	output, _ := cmd.Root().Flags().GetString("output")
 
 	if len(charger) == 0 {
-		return fmt.Errorf("could not find warp charger address")
+		//load from config
+		charger = viper.GetString("charger.url")
+		if len(charger) == 0 {
+			return fmt.Errorf("could not find warp charger address as argument or part of the configuration file %s", viper.ConfigFileUsed())
+		}
+	}
+
+	// load user and pass from config when empty, empty values are fine
+	if len(user) == 0 {
+		user = viper.GetString("charger.username")
+	}
+
+	if len(pass) == 0 {
+		pass = viper.GetString("charger.password")
 	}
 
 	req(charger, user, pass, output)
